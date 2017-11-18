a7ca75271277aed0fa67594e755a7f4bd0021368
Naushad Alam <Naushad.Alam@ehi.com>
2017-11-19 00:46:34 +0530

    Added support for XML response

--- src/main/java/microservice_spring_boot/controller/ReservationController.java
+++ src/main/java/microservice_spring_boot/controller/ReservationController.java
@@ -1,12 +1,12 @@
 package microservice_spring_boot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import microservice_spring_boot.bo.ReservationFulfillment;
@@ -19,17 +19,18 @@ import microservice_spring_boot.service.impl.ReservationServiceImpl;
 @RestController
 @SpringBootApplication 
 @ComponentScan("microservice_spring_boot")
-@RequestMapping("/res")
+@RequestMapping("/reservation")
 public class ReservationController {
 	
 	@Autowired(required=true)
 	ReservationServiceImpl reservationServiceImpl;
 	
-	@RequestMapping(value="/create", produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value="/create", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@ResponseBody
 	ReservationFulfillment createRes() {
 		return reservationServiceImpl.createReservation();
 	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationController.class, args);
 	}
