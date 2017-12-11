ac32047cd38a307678cc630c8d35a8a238aea933
naushad <naushad.jgec@gmail.com>
2017-12-11 20:49:08 +0530

    Added NextGen App

---
+++ gateway/src/main/java/com/cts/product/gateway/Application.java
@@ -0,0 +1,20 @@
+package com.cts.product.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(
+	            Application.class) 
+	            .web(true).run(args);
+	}
+	
+	/*@Bean
+	public GatewayPreFilter getwayPreFilter() {
+		return new GatewayPreFilter();
+	}*/
+}
